059a3a7fb7e5f377db1dd1ef4a4958971a0ad247
ansrlgur12 <ansrlgur12@naver.com>
2025-11-28 15:17:30 +0900

    cors 설정 완료

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',  // 개발 환경
+      'https://blog-api.kihyuk.site',  // 프로덕션 프론트엔드 도메인
+    ],
+    credentials: true,  // JWT 토큰 전송을 위해 필요
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
